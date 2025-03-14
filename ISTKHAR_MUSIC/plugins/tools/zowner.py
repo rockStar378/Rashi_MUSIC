7c8b8eb03a70d7b35fe58a405b944e0aa0f9aa9f
𐏓꯭꯭꯭꯭꯭꯭꯭𝖳 ꯭꯭ʜ꯭꯭꯭ ߎ꯭ ꯭ɴ ꯭ᴅ꯭ ᴇ꯭ ʀ ꯭ꭗ꯭Ḋ꯭꯭ <mdistkhar601@gmail.com>
2025-03-15 00:59:48 +0530

    Update zowner.py

--- ISTKHAR_MUSIC/plugins/tools/zowner.py
+++ ISTKHAR_MUSIC/plugins/tools/zowner.py
@@ -11,7 +11,7 @@ from ISTKHAR_MUSIC.utils.database import add_served_chat, get_assistant
 @app.on_message(filters.command("repo"))
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1aac9a42f6f35138da34b.jpg",
+        photo=f"https://files.catbox.moe/q9shgz.jpg",
         caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
         reply_markup=InlineKeyboardMarkup(
             [
