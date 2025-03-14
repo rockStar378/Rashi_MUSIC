fc7a84a058c9e0eb496a81c89cb11da3ae7f97c2
𐏓꯭꯭꯭꯭꯭꯭꯭𝖳 ꯭꯭ʜ꯭꯭꯭ ߎ꯭ ꯭ɴ ꯭ᴅ꯭ ᴇ꯭ ʀ ꯭ꭗ꯭Ḋ꯭꯭ <mdistkhar601@gmail.com>
2025-03-14 23:01:41 +0530

    Add files via upload

---
+++ ISTKHAR_MUSIC/plugins/tools/ping.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from ISTKHAR_MUSIC import app
+from ISTKHAR_MUSIC.core.call import noor
+from ISTKHAR_MUSIC.utils import bot_sys_stats
+from ISTKHAR_MUSIC.utils.decorators.language import language
+from ISTKHAR_MUSIC.utils.inline import supp_markup
+from config import BANNED_USERS, PING_IMG_URL
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await noor.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
