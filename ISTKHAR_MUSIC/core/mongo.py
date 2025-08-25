bbe7c70822a32a7eb02811300e9f9b80614ffd13
ISTKHAR-king <istkharalam225@gmail.com>
2025-08-25 17:15:11 +0530

    Update mongo.py

--- ISTKHAR_MUSIC/core/mongo.py
+++ ISTKHAR_MUSIC/core/mongo.py
@@ -28,5 +28,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.ISTKHAR_MUSIC
-    pymongodb = _mongo_sync_.ISTKHAR_MUSIC
+    mongodb = _mongo_async_.Yukki
+    pymongodb = _mongo_sync_.Yukki
