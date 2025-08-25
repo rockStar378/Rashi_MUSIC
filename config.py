fbe72288f572953cdac7d81442f4e07dcf0ceada
ISTKHAR-king <istkharalam225@gmail.com>
2025-08-25 16:55:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots901703') # youtube song api ke 
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/ISTKHAR-king/ISTKHAR_MUSIC",
