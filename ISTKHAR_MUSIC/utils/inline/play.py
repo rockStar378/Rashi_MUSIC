02d76279da4ca45f8a3bc49430bdd6f4773eccab
rockStar378 <shahzadluqman092@gmail.com>
2025-09-06 11:33:00 +0530

    Update play.py

--- ISTKHAR_MUSIC/utils/inline/play.py
+++ ISTKHAR_MUSIC/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"https://t.me/ll_ISTKHAR_BABY_lll"
+                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"https://t.me/ANKIT_PAWOR"
             ),
             InlineKeyboardButton(
-                text="🥀 ᴄʜᴀᴛ 🥀", url=f"https://t.me/+--i-JpgGkc8wY2E9"
+                text="🥀 ᴄʜᴀᴛ 🥀", url=f"https://t.me/+eDGuiLMGvO0zNmFl"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"https://t.me/ll_ISTKHAR_BABY_lll"
+                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"https://t.me/ANKIT_PAWOR"
             ),
             InlineKeyboardButton(
-                text="🥀 ᴄʜᴀᴛ 🥀", url=f"https://t.me/+--i-JpgGkc8wY2E9"
+                text="🥀 ᴄʜᴀᴛ 🥀", url=f"https://t.me/+eDGuiLMGvO0zNmFl"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
