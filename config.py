870a46c602fd6bcaac601aaadf2d8fabf7b86c06
rockStar378 <shahzadluqman092@gmail.com>
2025-12-24 11:03:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'http://riyabots.site') #youtube song url
+API_URL = getenv("API_URL", 'https://deadlinetech.site') #youtube song url
 API_KEY = getenv("API_KEY", 'StrangerApic0b57ec') # youtube song api ke 
  
 UPSTREAM_REPO = getenv(
