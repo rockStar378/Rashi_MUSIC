2eaa71543f266f8b4d23b1bc5f08dca702e3335b
rockStar378 <shahzadluqman092@gmail.com>
2026-08-06 11:21:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ YTPROXY_URL = os.getenv("YTPROXY_URL", "https://tgapi.xbitcode.com")
 YT_API_KEY = os.getenv("YT_API_KEY" , "xbit_kp3GFnAvdnFVDV3L6xACy-jbVBE5q5Cd")
 
 # --- API 3: Worker ---
-API_TOKEN = 'xbit_kp3GFnAvdnFVDV3L6xACy-jbVBE5q5Cd'
-BASE_URL = 'http://api.nubcoders.com'
+WORKER_FALLBACK_API_URL = os.getenv("WORKER_FALLBACK_API_URL", "http://api.nubcoders.com")
+WORKER_FALLBACK_API_KEY = os.getenv("WORKER_FALLBACK_API_KEY", "xbit_kp3GFnAvdnFVDV3L6xACy-jbVBE5q5Cd")
 
 # --- API 4: Inflex ---
 INFLEX_API_URL = os.getenv("INFLEX_API_URL", "https://teaminflex.xyz")
